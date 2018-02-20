853b17f9dae2396280dcc5ef92419f4e7d03d10b
Jake Mathews <mathewsj2@wit.edu>
2018-02-20 10:35:08 -0500

    Initial commit

---
+++ src/main/java/io/graydragon/horizonsdatarequest/HorizonsDataRequestApplication.java
@@ -0,0 +1,12 @@
+package io.graydragon.horizonsdatarequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonsDataRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonsDataRequestApplication.class, args);
+	}
+}
